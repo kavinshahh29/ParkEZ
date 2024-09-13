5c2c481b0383b019ea1c42fd9839c79bd0267f55
Kavin <kavinshah290304@gmail.com>
2024-09-13 14:33:17 +0530

    Save current changes before revert

--- server/models/Parking.js
+++ server/models/Parking.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Parking Collection
 // {
 // "_id": "ObjectId",
@@ -14,56 +13,54 @@
 // "description": "String"
 // }
 
+// const mongoose = require("mongoose");
+
+// const parkingSchema = new mongoose.Schema({
+//   parking_id: "String",
+//   address: "String",
+//   location: {
+//     type: Object({ type: String, coordinates: [Number, Number] }),
+//     required: true,
+//   },
+//   photo_URL: "String",
+//   video_URL: "String",
+//   owner_id: {
+//     type: mongoose.Schema.ObjectId,
+//     ref: "User",
+//   },
+//   description: "String",
+//   is_available: {
+//     type: Boolean,
+//     default: true, // True means parking is available for bookings
+//   },
+//   is_verified: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
+
+// module.exports = mongoose.model("Parking", parkingSchema);
+
 const mongoose = require("mongoose");
 
 const parkingSchema = new mongoose.Schema({
-  parking_id: "String",
-  address: "String",
-  location: {
-    type: Object({ type: String, coordinates: [Number, Number] }),
-    required: true,
+  address: String,
+  location_type: String,
+  location_coordinates: {
+    lat: Number,
+    log: Number,
   },
-  photo_URL: "String",
-  video_URL: "String",
+  photo_URL: String,
+  video_URL: String,
   owner_id: {
     type: mongoose.Schema.ObjectId,
     ref: "User",
   },
-  description: "String",
-  is_available: {
-    type: Boolean,
-    default: true, // True means parking is available for bookings
-  },
-  is_verified: {
+  description: String,
+  isVerified: {
     type: Boolean,
     default: false,
   },
 });
 
 module.exports = mongoose.model("Parking", parkingSchema);
-=======
-const mongoose=require("mongoose");
-
-const parkingSchema= new mongoose.Schema({
-    
-    address:String,
-    location_type : String ,
-    location_coordinates : {
-        lat : Number ,
-        log : Number
-    },
-    photo_URL:String,
-    video_URL:String,
-    owner_id:{
-        type:mongoose.Schema.ObjectId,
-        ref:"User"
-    },
-    description:String ,
-    isVerified : {
-        type : Boolean ,
-        default : false ,
-    }
-})
-
-module.exports = mongoose.model('Parking' , parkingSchema) ;
->>>>>>> 095aa68d10f17571970f7d0933d4cdc257d9c20d
--- server/routes/Parking.js
+++ server/routes/Parking.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
+// const {
+//   addParking,
+//   removeParking,
+//   verifyParking,
+//   updateParking,
+// } = require("../controllers/Parking");
+// const { isAuthenticated } = require("../middlewares/auth");
+
+// router.post("/addParking", isAuthenticated, addParking);
+// router.post("/removeParking", isAuthenticated, removeParking);
+// router.post("/verifyParking", isAuthenticated, verifyParking);
+// router.post("/updateParking", isAuthenticated, updateParking);
+
 const {
   addParking,
   removeParking,
-  verifyParking,
-  updateParking,
-} = require("../controllers/Parking");
-const { isAuthenticated } = require("../middlewares/auth");
-
-router.post("/addParking", isAuthenticated, addParking);
-router.post("/removeParking", isAuthenticated, removeParking);
-router.post("/verifyParking", isAuthenticated, verifyParking);
-router.post("/updateParking", isAuthenticated, updateParking);
-=======
-const { addParking, removeParking, getAllParkings } = require("../controllers/Parking.js");
+  getAllParkings,
+} = require("../controllers/Parking.js");
 const { isAuthenticated } = require("../middlewares/auth.js");
 
 router.post("/parking", isAuthenticated, addParking);
 router.delete("/parking", isAuthenticated, removeParking);
-router.get("/parkings" , isAuthenticated  , getAllParkings );
->>>>>>> 095aa68d10f17571970f7d0933d4cdc257d9c20d
+router.get("/parkings", isAuthenticated, getAllParkings);
 
 module.exports = router;
