979a2b1f5abf2c793723d4413e435c198e5faffa
Kavin <kavinshah290304@gmail.com>
2024-09-20 20:07:43 +0530

    fix

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 
 const dbConnection = async () => {
   try {
-    const connection = await mongoose.connect(
-      "mongodb+srv://kriscollege:Kris%40123@cluster0.3crjfvn.mongodb.net/"
-    );
+    const connection = await mongoose.connect(process.env.MONGO_URL);
     console.log(`Database connected : ${connection.connection.host}`);
   } catch (err) {
     console.error("Error while connectin database ,", err);
